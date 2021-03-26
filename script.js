32dbc0757fef64c30e06fd4e14791f7e194e1e9f
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-26 13:36:47 +0000

    Unit7-07: Multiplication in Addition

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// setup Variables
 document.getElementById('multiply').addEventListener('click', calculate)
 
 let firstNum = 0
@@ -5,13 +6,14 @@ let secondNum = 0
 let counter = 0
 let answer = 0
 
+//calculate multiplication in addition
 function calculate () {
   firstNum = document.getElementById('first').value
   secondNum = document.getElementById('second').value
   firstNum = parseInt(firstNum)
   secondNum = parseInt(secondNum)
 
-  for (counter = 0; counter < firstNum; counter++) {
+  for (counter = 0; counter < firstNum; counter ++) {
     answer = answer + secondNum
   }
   document.getElementById('answer').innerHTML = answer
